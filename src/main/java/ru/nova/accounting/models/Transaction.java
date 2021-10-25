3b0fa0eb73770fbc39500f892efde86d7c0736e2
asgorlov <asgorlov@outlook.com>
2021-10-25 19:17:16 +0300

    Added repo and minor changes

--- src/main/java/ru/nova/accounting/models/Transaction.java
+++ src/main/java/ru/nova/accounting/models/Transaction.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -22,4 +23,30 @@ public class Transaction implements Serializable {
     private int value;
     private String comment;
     private String iconUrl;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Transaction)) return false;
+
+        Transaction that = (Transaction) o;
+
+        if (value != that.value) return false;
+        if (!id.equals(that.id)) return false;
+        if (!Objects.equals(date, that.date)) return false;
+        if (!Objects.equals(type, that.type)) return false;
+        if (!Objects.equals(comment, that.comment)) return false;
+        return Objects.equals(iconUrl, that.iconUrl);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        result = 31 * result + value;
+        result = 31 * result + (comment != null ? comment.hashCode() : 0);
+        result = 31 * result + (iconUrl != null ? iconUrl.hashCode() : 0);
+        return result;
+    }
 }
